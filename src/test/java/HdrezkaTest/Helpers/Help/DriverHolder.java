2c7fa9b0a99bfdd4f7d869af9580b78d3bfd502f
Tw1stTheBones <molokomax93@gmail.com>
2021-04-30 16:26:22 +0300

    second commit

---
+++ src/test/java/HdrezkaTest/Helpers/Help/DriverHolder.java
@@ -0,0 +1,18 @@
+package HdrezkaTest.Helpers.Help;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class DriverHolder {
+    protected WebDriver driver;
+
+    public DriverHolder(WebDriver driver) {
+        this.driver = driver;
+
+    }
+
+
+}
+
---
+++ src/test/java/HdrezkaTest/Helpers/ObjetUpPage/MoiZakladkiHelper.java
@@ -0,0 +1,10 @@
+package HdrezkaTest.Helpers.ObjetUpPage;
+
+import org.openqa.selenium.WebDriver;
+
+public class MoiZakladkiHelper extends MoiZakladkiElements{
+    public MoiZakladkiHelper(WebDriver driver){
+        super(driver);
+    }
+
+}
